381d715657af85dd8236316ff7167041deb546e8
dominikhuser <https://doomii96@bitbucket.org/JFltr/translater.git>
2019-10-08 14:32:42 +0200

    initial commit

---
+++ src/main/java/bpm/matcher/App.java
@@ -0,0 +1,13 @@
+package bpm.matcher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
