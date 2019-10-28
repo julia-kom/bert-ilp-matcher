376b6e5af54a8d71928971cb4c751231a0799f3d
dominikhuser <https://doomii96@bitbucket.org/JFltr/translater.git>
2019-10-28 17:24:12 +0100

    make jar runnable

--- rwth-cluster-shell/batch-test.sh
+++ rwth-cluster-shell/batch-test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # run batch test
-java -cp ilp-profile-matcher-1.0-SNAPSHOT.jar Evaluation -batch -gold-standard-path ./eval-data/goldstandard/birth -net-path ./eval-data/pnml/birth -ilp Relaxed -pre-match
+java -jar ./ilp-profile-matcher-1.0-SNAPSHOT-jar-with-dependencies.jar  -batch -gold-standard-path ../eval-data/goldstandard/birth -net-path ../eval-data/pnml/birth -ilp Relaxed -pre-match
