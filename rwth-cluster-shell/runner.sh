aa06a5524aaca20f3db5637bd9246d5025c9d21a
dominikhuser <https://doomii96@bitbucket.org/JFltr/translater.git>
2019-10-31 17:01:05 +0100

    readme.md

--- rwth-cluster-shell/runner.sh
+++ rwth-cluster-shell/runner.sh
@@ -1,15 +1,11 @@
-#!/usr/bin/env zsh
-#BSUB -J TEST
-#BSUB -o ouput.txt
-#BSUB -n 2
-#BSUB -W 15
-#BSUB -M 700
-#BSUB -a openmp
-#BSUB -u dominik.hueser@rwth-aachen.de
-#BSUB -N
-module switch intel gcc/6
+#!/usr/local_rwth/bin/zsh
+# ask for 10 GB memory
+#SBATCH --mem-per-cpu=10240M   #M is the default and can therefore be omitted, but could also be K(ilo)|G(iga)|T(era)
+# name the job
+#SBATCH --job-name=BATCH_TEST
+# declare the merged STDOUT/STDERR file
+#SBATCH --output=output.%J.txt
 
+### begin of executable commands
 #Add Gurobi
-module load MATH
-module load gurobi
 sh batch-test.sh
