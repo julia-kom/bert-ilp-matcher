cf207b72000db854c149b4df6d44b4cb05f46991
dominikhuser <https://doomii96@bitbucket.org/JFltr/translater.git>
2019-11-07 15:26:10 +0100

    runnable sh

--- rwth-cluster-shell/batch-test.sh
+++ rwth-cluster-shell/batch-test.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
-
+#!/usr/local_rwth/bin/zsh
+# don't forget to >> chmod +x ./batch-test.sh
+#set missing links
+module load MATH
+module load gurobi/8.0.0
 # run batch test for uni
 java -jar ./ilp-profile-matcher-1.0-SNAPSHOT-jar-with-dependencies.jar -batch -gold-standard-path $HOME/eval-data/goldstandard/birth -net-path $HOME/eval-data/pnml/birth -ilp Relaxed -pre-match
