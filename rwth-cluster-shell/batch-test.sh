a0bb1ed87148e6f0a0f4d9f31c3a64309820f3fe
dominikhuser <https://doomii96@bitbucket.org/JFltr/translater.git>
2019-11-15 10:34:20 +0100

    set server params

--- rwth-cluster-shell/batch-test.sh
+++ rwth-cluster-shell/batch-test.sh
@@ -1,10 +1,12 @@
 #!/usr/local_rwth/bin/zsh
 #SBATCH -t 0-01:00:00 # wallclock time
-#SBATCH -A "lp tests" # Account
-#SBATCH -J "batch gold-standard-path $HOME/eval-data/goldstandard/birth net-path $HOME/eval-data/pnml/birth ilp Relaxed pre-match" # Job name
-# don't forget to >> chmod +x ./batch-test.sh
+#SBATCH -N 1 1 #min max nodes needed
+#SBATCH -n 1 #number of tasks to be launched
+#SBATCH --mem-per-cpu= 1GB # Memory per CPU in MB
+#SBATCH --cpu-per-task=8 # CPU per task
+
 #set missing links
 module load MATH
 module load gurobi/8.0.0
 # run batch test for uni
-java -jar ./ilp-profile-matcher-1.0-SNAPSHOT-jar-with-dependencies.jar -batch -gold-standard-path $HOME/eval-data/goldstandard/birth -net-path $HOME/eval-data/pnml/birth -ilp Relaxed -pre-match
+java -jar ./ilp-profile-matcher-1.0-SNAPSHOT-jar-with-dependencies.jar -batch -gold-standard-path $HOME/eval-data/goldstandard/birth -net-path $HOME/eval-data/pnml/birth -ilp Relaxed3 -pre-match
