1ddba965f109430f81e9edfdf1dc927a08964d28
dominikhuser <https://doomii96@bitbucket.org/JFltr/translater.git>
2019-11-19 15:25:37 +0100

    sh scripts

--- rwth-cluster-shell/batch-birth-test.sh
+++ rwth-cluster-shell/batch-birth-test.sh
@@ -1,13 +1,12 @@
 #!/usr/local_rwth/bin/zsh
-#parameters: $1 weight, $2 ilp
+#parameters: $1 weight, $2 ilp, $3 word-sim
 #setup slurm environment
-
 #SBATCH -t 0-02:00:00 # wallclock time
 #SBATCH --exclusive #exclusive use
 #SBATCH --partition c18m #choose the partition (https://doc.itc.rwth-aachen.de/display/CC/Hardware+of+the+RWTH+Compute+Cluster)
 #SBATCH -N 1 #min max nodes needed
 #SBATCH -n 1 #number of tasks to be launched
-#SBATCH --mem-per-cpu= 4GB # Memory per CPU in MB
+#SBATCH --mem-per-cpu=4GB # Memory per CPU in MB
 #SBATCH --cpus-per-task=4 # CPU per task
 #SBATCH -o ./slurm-report/output_%j.txt #output
 
@@ -17,4 +16,4 @@ module load MATH
 module load gurobi/8.0.0
 
 # run batch test for uni
-java -jar ./ilp-profile-matcher-1.0-SNAPSHOT-jar-with-dependencies.jar -batch -gold-standard-path $HOME/eval-data/goldstandard/birth -net-path $HOME/eval-data/pnml/birth -similarity-weight $1 -ilp $2 -pre-match
+java -jar ./ilp-profile-matcher-1.0-SNAPSHOT-jar-with-dependencies.jar -batch -gold-standard-path $HOME/eval-data/goldstandard/birth -net-path $HOME/eval-data/pnml/birth -similarity-weight $1 -ilp $2 -pre-match -word-sim $3
--- rwth-cluster-shell/batch-test.sh
+++ rwth-cluster-shell/batch-test.sh
@@ -1,16 +1,18 @@
 #!/usr/local_rwth/bin/zsh
-#parameters $1 is ilp
-#parameters stepsize $2..$n
+#parameters $1 is ilp = {Basic, Relaxed, Relaxed2, Relaxed3}
+#parameters $2 is word similarity = {Lin, Levenshtein, Jiang, Levenshtein-Lin-Max, Levenshtein-Jiang-Max}
+#parameters stepsize is $3..$n
 ilp=$1
+word=$2
 
 #start all birth jobs
-for stepsize in "$@"
+for stepsize in "${@:3}";
 do
-  sbatch batch-birth-test.sh --export=1=$stepsize,2=$ilp
+  sbatch batch-birth-test.sh --export=1=$stepsize,2=$ilp,3=$word
 done
 
 #start all uni jobs
-for stepsize in "$@"
+for stepsize in "${@:3}";
 do
-  sbatch batch-uni-test.sh --export=1=$stepsize,2=$ilp
+  sbatch batch-uni-test.sh --export=1=$stepsize,2=$ilp,3=$word
 done
