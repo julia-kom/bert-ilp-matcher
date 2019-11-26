d177bab62573ad03c370efbb4b184dbf6e90f0a6
dominikhuser <https://doomii96@bitbucket.org/JFltr/translater.git>
2019-11-26 14:53:36 +0100

    change bash scripts

--- rwth-cluster-shell/batch-birth-test.sh
+++ rwth-cluster-shell/batch-birth-test.sh
@@ -1,5 +1,5 @@
 #!/usr/local_rwth/bin/zsh
-#parameters: $1 weight, $2 ilp, $3 word-sim
+#parameters: $1 weight, $2 ilp, $3 word-sim, $4 ilp-time-limit
 #setup slurm environment
 #SBATCH -t 0-02:00:00 # wallclock time
 ##SBATCH --exclusive #exclusive use
@@ -16,4 +16,4 @@ module load MATH
 module load gurobi/8.0.0
 
 # run batch test for uni
-java -jar ./ilp-profile-matcher-1.0-SNAPSHOT-jar-with-dependencies.jar -batch -gold-standard-path $HOME/eval-data/goldstandard/birth -net-path $HOME/eval-data/pnml/birth -similarity-weight $1 -ilp $2 -pre-match -word-sim $3
+java -jar ./ilp-profile-matcher-1.0-SNAPSHOT-jar-with-dependencies.jar -batch -gold-standard-path $HOME/eval-data/goldstandard/birth -net-path $HOME/eval-data/pnml/birth -similarity-weight $1 -ilp $2 -pre-match -word-sim $3 -ilp-time-limit $4
--- rwth-cluster-shell/batch-test.sh
+++ rwth-cluster-shell/batch-test.sh
@@ -1,18 +1,20 @@
 #!/usr/local_rwth/bin/zsh
 #parameters $1 is ilp = {BASIC, RELAXED, RELAXED2, RELAXED3}
 #parameters $2 is word similarity = {LIN, LEVENSHTEIN, JIANG, LEVENSHTEIN_LIN_MAX, LEVENSHTEIN_JIANG_MAX}
-#parameters stepsize is $3..$n
+#parameters $3 ilp-time-limit
+#parameters stepsize is $4..$n
 ilp=$1
 word=$2
+time=$3
 
 #start all birth jobs
-for stepsize in "${@:3}";
+for stepsize in "${@:4}";
 do
-  sbatch batch-birth-test.sh $stepsize $ilp $word
+  sbatch batch-birth-test.sh $stepsize $ilp $word $time
 done
 
 #start all uni jobs
-for stepsize in "${@:3}";
-do
-  sbatch batch-uni-test.sh $stepsize $ilp $word
-done
+#for stepsize in "${@:3}";
+#do
+#  sbatch batch-uni-test.sh $stepsize $ilp $word $time
+#done
