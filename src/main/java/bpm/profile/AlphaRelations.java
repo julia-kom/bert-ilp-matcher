e8669ffa5436585c9de23a8b6c5bd618cccfc0d1
dominikhuser <https://doomii96@bitbucket.org/JFltr/translater.git>
2020-01-10 15:13:05 +0100

    rewrite ARP

--- src/main/java/bpm/profile/AlphaRelations.java
+++ src/main/java/bpm/profile/AlphaRelations.java
@@ -1,10 +1,6 @@
 package bpm.profile;
 
 import bpm.matcher.Preprocessor;
-import org.jbpt.bp.BehaviouralProfile;
-import org.jbpt.bp.RelSetType;
-import org.jbpt.bp.construct.AbstractRelSetCreator;
-import org.jbpt.bp.construct.RelSetCreator;
 import org.jbpt.petri.NetSystem;
 import org.jbpt.petri.Node;
 import org.jbpt.petri.PetriNet;
@@ -15,33 +11,13 @@ import java.util.*;
 
 public class AlphaRelations extends AbstractProfile {
 
-    private BehaviouralProfile<NetSystem, Node> relations;
-    private static AlphaCreatorNet creator;
+    private NetSystem net;
 
     public AlphaRelations(NetSystem net){
-        if(creator == null){
-            creator = new AlphaCreatorNet();
-        }
-        relations = creator.deriveRelationSet(net);
+        if (!PetriNet.STRUCTURAL_CHECKS.isWorkflowNet(net)) throw new IllegalArgumentException();
+        this.net = net;
     }
 
-    @Override
-    public Relation getRelationForEntities(Node n1, Node n2) {
-        RelSetType rel = relations.getRelationForEntities(n1, n2);
-        switch(rel){
-            case Order:
-                return Relation.ALPHA_ORDER;
-            case Exclusive:
-                return Relation.ALPHA_EXCLUSIVE;
-            case Interleaving:
-                return Relation.ALPHA_INTERLEAVING;
-            case ReverseOrder:
-                return Relation.ALPHA_REVERSE_ORDER;
-            case None:
-                return Relation.NONE;
-        }
-        return null;
-    }
 
     @Override
     public double getRelationSimilarity(Relation r1, Relation r2) {
@@ -52,138 +28,79 @@ public class AlphaRelations extends AbstractProfile {
         }
     }
 
+    //TODO write this.
     @Override
     public String toString(){
-        return relations.toString();
+        return "";
     }
+
+    @Override
+    public Relation getRelationForEntities(Node n1, Node n2) {
+        // Implementation is similar to BP but this time, with directly follows instead of eventually follows as ground.
+        ConcurrencyRelation concurrencyRelation = new ConcurrencyRelation(net);
+        if(concurrencyRelation.areConcurrent(n1,n2) || (directlyFollows(n1,n2) && directlyFollows(n2,n1))){
+            //either two transitions are concurrent or they are in a 1-loop
+            return Relation.ALPHA_INTERLEAVING;
+        }else if(directlyFollows(n1,n2) && !directlyFollows(n2,n1)) {
+            //n1 can follow n2 but n2 never follow n1
+            return Relation.ALPHA_ORDER;
+        }else if(!directlyFollows(n1,n2) && directlyFollows(n2,n1)) {
+            //n2 can follow n1 but never the other way around
+            return Relation.ALPHA_REVERSE_ORDER;
+        }else{
+            return Relation.ALPHA_EXCLUSIVE;
+        }
+    }
+
 
     /**
-     * Structure as in BPCreatorNet.java in JBPT Lib
-     * What has changed? An additional requeirment for a relation is added into the if-else part assigning relations to
-     * an entry in the relational matrix: Direct Neighboor porperty must be satisfied too.
+     * Returns true if two transitions are directly following each other
+     * Or there is a path of only tau transitions between these transitions.
+     *
+     * Idea check all transitions which are directly following. If one of the transitions is tau check the
+     * follow up transitions of that tau transition too.
+     * Break at already visited transitions to prevent infinite loops
+     *
+     * @param n1
+     * @param n2
+     * @return
      */
-    private class AlphaCreatorNet extends AbstractRelSetCreator implements RelSetCreator<NetSystem, Node> {
-
-
-        private AlphaCreatorNet() {
-
-
-        }
-
-        public BehaviouralProfile<NetSystem, Node> deriveRelationSet(NetSystem pn) {
-            return deriveRelationSet(pn, pn.getNodes());
-        }
-
-        public BehaviouralProfile<NetSystem, Node> deriveRelationSet(NetSystem pn, Collection<Node> nodes) {
-
-            /*
-             * Check some of the assumptions.
-             */
-            //if (!PetriNet.StructuralClassChecks.isExtendedFreeChoice(pn)) throw new IllegalArgumentException();
-            if (!PetriNet.STRUCTURAL_CHECKS.isWorkflowNet(pn)) throw new IllegalArgumentException();
-
-            BehaviouralProfile<NetSystem, Node> profile = new BehaviouralProfile<NetSystem, Node>(pn,nodes);
-            RelSetType[][] matrix = profile.getMatrix();
-
-            ConcurrencyRelation concurrencyRelation = new ConcurrencyRelation(pn);
-
-            for(Node n1 : profile.getEntities()) {
-                int index1 = profile.getEntities().indexOf(n1);
-                for(Node n2 : profile.getEntities()) {
-                    int index2 = profile.getEntities().indexOf(n2);
-                    /*
-                     * The matrix is symmetric. Therefore, we need to traverse only
-                     * half of the entries.
-                     */
-                    if (index2 > index1)
-                        continue;
-                    /*
-                     * What about the relation of a node to itself?
-                     */
-                    if (index1 == index2) {
-                        if (PetriNet.DIRECTED_GRAPH_ALGORITHMS.hasPath(pn,n1,n2))
-                            matrix[index1][index1] = RelSetType.Interleaving;
-                        else
-                            matrix[index1][index1] = RelSetType.Exclusive;
-                    }
-                    /*
-                     * Check all cases for two distinct nodes of the net
-                     */
-                    //todo change conditions. That is the only thing to change! Only thing new: check if direct neighbors i.e. only tau transitions are on a path from source to target.
-                    else if(directlyFollows(pn,n1,n2) || directlyFollows(pn,n2,n1)){
-                        if (PetriNet.DIRECTED_GRAPH_ALGORITHMS.hasPath(pn,n1,n2) && PetriNet.DIRECTED_GRAPH_ALGORITHMS.hasPath(pn,n2,n1)) {
-                            super.setMatrixEntry(matrix,index1,index2,RelSetType.Interleaving);
-                        }
-                        else if (concurrencyRelation.areConcurrent(index1,index2)) {
-                            super.setMatrixEntry(matrix,index1,index2,RelSetType.Interleaving);
-                        }
-                        else if (!concurrencyRelation.areConcurrent(index1,index2) && !PetriNet.DIRECTED_GRAPH_ALGORITHMS.hasPath(pn,n1,n2) && !PetriNet.DIRECTED_GRAPH_ALGORITHMS.hasPath(pn,n2,n1)) {
-                            super.setMatrixEntry(matrix,index1,index2,RelSetType.Exclusive);
-                        }
-                        else if (PetriNet.DIRECTED_GRAPH_ALGORITHMS.hasPath(pn,n1,n2) && !PetriNet.DIRECTED_GRAPH_ALGORITHMS.hasPath(pn,n2,n1)) {
-                            super.setMatrixEntryOrder(matrix,index1,index2);
-                        }
-                        else if (PetriNet.DIRECTED_GRAPH_ALGORITHMS.hasPath(pn,n2,n1) && !PetriNet.DIRECTED_GRAPH_ALGORITHMS.hasPath(pn,n1,n2)) {
-                            super.setMatrixEntryOrder(matrix,index2,index1);
-                        }
-                    }else{
-                        super.setMatrixEntry(matrix,index1,index2,RelSetType.Exclusive);
-                    }
-                }
-            }
-
-            return profile;
-        }
-
-        /**
-         * Returns true if two transitions are directly following each other
-         * Or there is a path of only tau transitions between these transitions.
-         *
-         * Idea check all transitions which are directly following. If one of the transitions is tau check the
-         * follow up transitions of that tau transition too.
-         * Break at already visited transitions to prevent infinite loops
-         *
-         * @param net
-         * @param n1
-         * @param n2
-         * @return
-         */
-        private boolean directlyFollows(NetSystem net, Node n1, Node n2) {
-            // places relation is irrelevant
-            if (!(n1 instanceof Transition && n2 instanceof Transition)) {
-                return false;
-            }
-
-            //tau transitions relation is irrelevant
-            if (Preprocessor.isTau((Transition) n1) || Preprocessor.isTau((Transition) n2)) {
-                return false;
-            }
-            Set<Transition> visited = new HashSet<>();
-            LinkedList<Transition> pending = new LinkedList<>();
-            pending.add((Transition) n1);
-            while (!pending.isEmpty()) {
-                // get first from linked list and put it into visited list
-                Transition current = pending.poll();
-                visited.add(current);
-
-                //fetch directly following transitions
-                Set<Transition> following = net.getPostsetTransitions(net.getPostset(current));
-
-                // check if n2 is found in the follow-up set
-                if (following.contains((Transition) n2)) {
-                    return true;
-                }
-
-                // add tau transitions in the follow up set to the pending set
-                for (Transition t : following) {
-                    if (Preprocessor.isTau(t) && !visited.contains(t)) {
-                        pending.add(t);
-                    }
-                }
-            }
-
+    private boolean directlyFollows( Node n1, Node n2) {
+        // places relation is irrelevant
+        if (!(n1 instanceof Transition && n2 instanceof Transition)) {
             return false;
         }
+
+        //tau transitions relation is irrelevant
+        if (Preprocessor.isTau((Transition) n1) || Preprocessor.isTau((Transition) n2)) {
+            return false;
+        }
+        Set<Transition> visited = new HashSet<>();
+        LinkedList<Transition> pending = new LinkedList<>();
+        pending.add((Transition) n1);
+        while (!pending.isEmpty()) {
+            // get first from linked list and put it into visited list
+            Transition current = pending.poll();
+            visited.add(current);
+
+            //fetch directly following transitions
+            Set<Transition> following = net.getPostsetTransitions(net.getPostset(current));
+
+            // check if n2 is found in the follow-up set
+            if (following.contains(n2)) {
+                return true;
+            }
+
+            // add tau transitions in the follow up set to the pending set
+            for (Transition t : following) {
+                if (Preprocessor.isTau(t) && !visited.contains(t)) {
+                    pending.add(t);
+                }
+            }
+        }
+
+        return false;
     }
+
 }
 
--- src/test/java/bpm/matcher/AlphaRelationTest.java
+++ src/test/java/bpm/matcher/AlphaRelationTest.java
@@ -122,4 +122,248 @@ public class AlphaRelationTest {
         Assert.assertSame(alpha.getRelationForEntities(t1,t4), AbstractProfile.Relation.ALPHA_ORDER);
     }
 
+
+    @Test
+    public void AlphaRelationLoopTest() {
+        NetSystem net1 = new NetSystem();
+
+        Transition t1 = new Transition();
+        t1.setId("t1");
+        t1.setName("Name 1");
+        t1.setLabel("Name 1");
+
+        Transition t2 = new Transition();
+        t2.setId("t2");
+        t2.setName("t2");
+        t2.setLabel("Name 2");
+
+        Transition t3 = new Transition();
+        t3.setId("t3");
+        t3.setName("Name 3");
+        t3.setLabel("Name 3");
+
+        Transition t4 = new Transition();
+        t4.setId("t4");
+        t4.setName("Name 4");
+        t4.setLabel("Name 4");
+
+        net1.addTransition(t1);
+        net1.addTransition(t2);
+        net1.addTransition(t3);
+        net1.addTransition(t4);
+
+        Place p1 = new Place("p1");
+        Place p2 = new Place("p2");
+        Place p3 = new Place("p3");
+        Place p4 = new Place("p4");
+
+
+        net1.addPlace(p1);
+        net1.addPlace(p2);
+        net1.addPlace(p3);
+        net1.addPlace(p4);
+
+        net1.addEdge(p1,t1);
+        net1.addEdge(t1,p2);
+        net1.addEdge(p2,t2);
+        net1.addEdge(t2,p3);
+        net1.addEdge(p3,t3);
+        net1.addEdge(t3,p4);
+
+        net1.addEdge(p3,t4);
+        net1.addEdge(t4,p2);
+
+        net1.putTokens(p1,1);
+
+        AlphaRelations alpha = new AlphaRelations(net1);
+
+        Assert.assertSame(alpha.getRelationForEntities(t1,t2), AbstractProfile.Relation.ALPHA_ORDER);
+        Assert.assertSame(alpha.getRelationForEntities(t2,t3), AbstractProfile.Relation.ALPHA_ORDER);
+
+        Assert.assertSame(alpha.getRelationForEntities(t2,t4), AbstractProfile.Relation.ALPHA_INTERLEAVING);
+        Assert.assertSame(alpha.getRelationForEntities(t4,t2), AbstractProfile.Relation.ALPHA_INTERLEAVING);
+
+        Assert.assertSame(alpha.getRelationForEntities(t1,t4), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+        Assert.assertSame(alpha.getRelationForEntities(t1,t3), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+        Assert.assertSame(alpha.getRelationForEntities(t4,t3), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+
+        Assert.assertSame(alpha.getRelationForEntities(t2,t2), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+        Assert.assertSame(alpha.getRelationForEntities(t4,t4), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+    }
+
+    @Test
+    public void AlphaRelationTwoTransitionLoopTest() {
+        NetSystem net1 = new NetSystem();
+
+        Transition t1 = new Transition();
+        t1.setId("t1");
+        t1.setName("Name 1");
+        t1.setLabel("Name 1");
+
+        Transition t2 = new Transition();
+        t2.setId("t2");
+        t2.setName("Name 2");
+        t2.setLabel("Name 2");
+
+        Transition t3 = new Transition();
+        t3.setId("t3");
+        t3.setName("Name 3");
+        t3.setLabel("Name 3");
+
+        Transition t4 = new Transition();
+        t4.setId("t4");
+        t4.setName("Name 4");
+        t4.setLabel("Name 4");
+
+        Transition t5 = new Transition();
+        t5.setId("t5");
+        t5.setName("Name 5");
+        t5.setLabel("Name 5");
+
+        net1.addTransition(t1);
+        net1.addTransition(t2);
+        net1.addTransition(t3);
+        net1.addTransition(t4);
+        net1.addTransition(t5);
+
+        Place p1 = new Place("p1");
+        Place p2 = new Place("p2");
+        Place p3 = new Place("p3");
+        Place p4 = new Place("p4");
+        Place p5 = new Place("p5");
+
+
+        net1.addPlace(p1);
+        net1.addPlace(p2);
+        net1.addPlace(p3);
+        net1.addPlace(p4);
+        net1.addPlace(p5);
+
+        net1.addEdge(p1,t1);
+        net1.addEdge(t1,p2);
+        net1.addEdge(p2,t2);
+        net1.addEdge(t2,p3);
+        net1.addEdge(p3,t3);
+        net1.addEdge(t3,p4);
+
+        net1.addEdge(p3,t4);
+        net1.addEdge(t4,p5);
+        net1.addEdge(p5,t5);
+        net1.addEdge(t5,p2);
+
+
+        net1.putTokens(p1,1);
+
+        AlphaRelations alpha = new AlphaRelations(net1);
+
+        Assert.assertSame(alpha.getRelationForEntities(t1,t2), AbstractProfile.Relation.ALPHA_ORDER);
+        Assert.assertSame(alpha.getRelationForEntities(t2,t3), AbstractProfile.Relation.ALPHA_ORDER);
+
+        Assert.assertSame(alpha.getRelationForEntities(t2,t4), AbstractProfile.Relation.ALPHA_ORDER);
+        Assert.assertSame(alpha.getRelationForEntities(t4,t5), AbstractProfile.Relation.ALPHA_ORDER);
+        Assert.assertSame(alpha.getRelationForEntities(t5,t2), AbstractProfile.Relation.ALPHA_ORDER);
+
+        Assert.assertSame(alpha.getRelationForEntities(t1,t4), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+        Assert.assertSame(alpha.getRelationForEntities(t1,t5), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+
+        Assert.assertSame(alpha.getRelationForEntities(t1,t3), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+        Assert.assertSame(alpha.getRelationForEntities(t4,t3), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+
+        Assert.assertSame(alpha.getRelationForEntities(t2,t2), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+        Assert.assertSame(alpha.getRelationForEntities(t4,t4), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+    }
+
+    @Test
+    public void AlphaConcurrentTest() {
+        NetSystem net1 = new NetSystem();
+
+        Transition t1 = new Transition();
+        t1.setId("t1");
+        t1.setName("Name 1");
+        t1.setLabel("Name 1");
+
+        Transition t2 = new Transition();
+        t2.setId("t2");
+        t2.setName("Name 2");
+        t2.setLabel("Name 2");
+
+        Transition t3 = new Transition();
+        t3.setId("t3");
+        t3.setName("Name 3");
+        t3.setLabel("Name 3");
+
+        Transition t4 = new Transition();
+        t4.setId("t4");
+        t4.setName("Name 4");
+        t4.setLabel("Name 4");
+
+        Transition t5 = new Transition();
+        t5.setId("t5");
+        t5.setName("Name 5");
+        t5.setLabel("Name 5");
+
+        net1.addTransition(t1);
+        net1.addTransition(t2);
+        net1.addTransition(t3);
+        net1.addTransition(t4);
+        net1.addTransition(t5);
+
+        Place p1 = new Place("p1");
+        Place p2 = new Place("p2");
+        Place p3 = new Place("p3");
+        Place p4 = new Place("p4");
+        Place p5 = new Place("p5");
+        Place p6 = new Place("p6");
+        Place p7 = new Place("p7");
+
+
+        net1.addPlace(p1);
+        net1.addPlace(p2);
+        net1.addPlace(p3);
+        net1.addPlace(p4);
+        net1.addPlace(p5);
+        net1.addPlace(p6);
+
+        net1.addEdge(p1,t1);
+
+        net1.addEdge(t1,p2);
+        net1.addEdge(t1,p3);
+
+        net1.addEdge(p2,t2);
+        net1.addEdge(t2,p4); // End branch 1
+
+        net1.addEdge(p3,t3);
+        net1.addEdge(t3,p5);
+
+        net1.addEdge(p5,t4);
+        net1.addEdge(t4,p6); // End branch 2
+
+        net1.addEdge(p4,t5);
+        net1.addEdge(p6,t5);
+
+        net1.addEdge(t5,p7);
+
+
+
+
+
+        net1.putTokens(p1,1);
+
+        AlphaRelations alpha = new AlphaRelations(net1);
+
+        Assert.assertSame(alpha.getRelationForEntities(t1,t2), AbstractProfile.Relation.ALPHA_ORDER);
+        Assert.assertSame(alpha.getRelationForEntities(t1,t3), AbstractProfile.Relation.ALPHA_ORDER);
+        Assert.assertSame(alpha.getRelationForEntities(t1,t4), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+        Assert.assertSame(alpha.getRelationForEntities(t1,t5), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+
+        Assert.assertSame(alpha.getRelationForEntities(t2,t3), AbstractProfile.Relation.ALPHA_INTERLEAVING);
+        Assert.assertSame(alpha.getRelationForEntities(t2,t4), AbstractProfile.Relation.ALPHA_INTERLEAVING);
+
+        Assert.assertSame(alpha.getRelationForEntities(t2,t5), AbstractProfile.Relation.ALPHA_ORDER);
+        Assert.assertSame(alpha.getRelationForEntities(t3,t5), AbstractProfile.Relation.ALPHA_EXCLUSIVE);
+
+        Assert.assertSame(alpha.getRelationForEntities(t2,t5), AbstractProfile.Relation.ALPHA_ORDER);
+        Assert.assertSame(alpha.getRelationForEntities(t4,t5), AbstractProfile.Relation.ALPHA_ORDER);
+    }
+
 }
