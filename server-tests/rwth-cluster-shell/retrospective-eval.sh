2b93dbb35c34922f6dfca233f06f7dc8461872bd
dominikhuser <https://doomii96@bitbucket.org/JFltr/translater.git>
2020-03-11 11:02:22 +0100

    update sh files to new structure

--- server-tests/rwth-cluster-shell/retrospective-eval.sh
+++ server-tests/rwth-cluster-shell/retrospective-eval.sh
@@ -5,6 +5,6 @@ do
     for d in $(find $1 -depth 1 -type d)
     do
         echo $d
-       java -jar ./ilp-profile-matcher-1.0-SNAPSHOT-jar-with-dependencies.jar -retrospective -gold-standard-path $2 -result-path $d -pp $thresh -eval-strat $3
+       java -jar ./ilp-profile-matcher-1.0-SNAPSHOT-jar-with-dependencies.jar eval -retrospective -gold-standard-path $2 -result-path $d -pp $thresh -eval-strat $3
     done
 done
--- server-tests/rwth-cluster-shell/single-test.sh
+++ server-tests/rwth-cluster-shell/single-test.sh
@@ -14,4 +14,4 @@ module load MATH
 module load gurobi/8.0.0
 
 # run batch test for uni
-java -jar ./ilp-profile-matcher-1.0-SNAPSHOT-jar-with-dependencies.jar -n1 $HOME/eval-data/pnml/birth/birthCertificate_p31.pnml -n2 $HOME/eval-data/pnml/birth/birthCertificate_p250.pnml -gs $HOME/eval-data/goldstandard/birth/birthCertificate_p31-birthCertificate_p250.rdf -ilp Relaxed3 -pre-match
+java -jar ./ilp-profile-matcher-1.0-SNAPSHOT-jar-with-dependencies.jar eval -n1 $HOME/eval-data/pnml/birth/birthCertificate_p31.pnml -n2 $HOME/eval-data/pnml/birth/birthCertificate_p250.pnml -gs $HOME/eval-data/goldstandard/birth/birthCertificate_p31-birthCertificate_p250.rdf -ilp Relaxed3 -pre-match
